54ce719940818e5d2b37ffdb85beb659d17b8848
Artem Rudenko <rtomrud@gmail.com>
2021-08-12 16:24:53 +0200

    Add tiny-tfidf to benchmark

--- bench.js
+++ bench.js
@@ -1,7 +1,7 @@
 import benchmark from "benchmark";
 import computeCosineSimilarity from "compute-cosine-similarity";
 import cosineSimilarity from "cosine-similarity";
-import { cosineSim } from "doc-similarity";
+import { Similarity } from "tiny-tfidf";
 import cosSimilarity from "./index.js";
 
 const { Suite } = benchmark;
@@ -18,9 +18,9 @@ while (dimension < dimensions) {
 
 Suite()
   .add("cos-similarity", () => cosSimilarity(a, b))
-  .add("doc-similarity cosineSim", () => cosineSim(a, b))
   .add("compute-cosine-similarity", () => computeCosineSimilarity(a, b))
   .add("cosine-similarity", () => cosineSimilarity(a, b))
+  .add("tiny-tfidf cosineSimilarity()", () => Similarity.cosineSimilarity(a, b))
   .on("cycle", (event) => console.log(String(event.target)))
   .on("complete", function () {
     console.log(`\nFastest is ${this.filter("fastest").map("name")}`);
