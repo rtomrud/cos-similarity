35496649f8385444557da5a6858ae16eef06cb93
Artem Rudenko <rtomrud@gmail.com>
2024-04-30 17:34:01 +0200

    Update the benchmark

--- README.md
+++ README.md
@@ -26,3 +26,28 @@ cosSimilarity([1, 2, 0], [1, 2, 0]); // -> 1
 cosSimilarity([2, 0, 0], [0, 2, 0]); // -> 0
 cosSimilarity([-1, -2, 0], [1, 2, 0]); // -> -1
 ```
+
+## Benchmark
+
+To run the benchmark, clone the repositry and run the bench script:
+
+```bash
+npm run bench
+```
+
+```
+benchmark                        time (avg)             (min … max)
+-------------------------------------------------------------------
+• cosine similarity modules
+-------------------------------------------------------------------
+cos-similarity                  249 ns/iter       (247 ns … 319 ns)
+compute-cosine-similarity       854 ns/iter       (829 ns … 428 µs)
+cosine-similarity            14'251 ns/iter    (13'680 ns … 229 µs)
+cosine-similarity-threshold     879 ns/iter       (802 ns … 310 µs)
+
+summary for cosine similarity modules
+  cos-similarity
+   3.43x faster than compute-cosine-similarity
+   3.53x faster than cosine-similarity-threshold
+   57.25x faster than cosine-similarity
+```
--- bench.js
+++ bench.js
@@ -1,29 +1,18 @@
-import console from "node:console";
-import benchmark from "benchmark";
 import computeCosineSimilarity from "compute-cosine-similarity";
 import cosineSimilarity from "cosine-similarity";
-import { Similarity } from "tiny-tfidf";
+import { cosineSimilarity as cosineSimilarityThreshold } from "cosine-similarity-threshold";
+import { bench, group, run } from "mitata";
 import cosSimilarity from "./index.js";
 
-const { Suite } = benchmark;
+const length = 100;
+const a = Array.from({ length }, () => Math.random());
+const b = Array.from({ length }, () => Math.random());
 
-const dimensions = 100;
-const a = [];
-const b = [];
-let dimension = 0;
-while (dimension < dimensions) {
-  a.push(Math.random());
-  b.push(Math.random());
-  dimension += 1;
-}
+group("cosine similarity modules", () => {
+  bench("cos-similarity", () => cosSimilarity(a, b));
+  bench("compute-cosine-similarity", () => computeCosineSimilarity(a, b));
+  bench("cosine-similarity", () => cosineSimilarity(a, b));
+  bench("cosine-similarity-threshold", () => cosineSimilarityThreshold(a, b));
+});
 
-Suite()
-  .add("cos-similarity", () => cosSimilarity(a, b))
-  .add("compute-cosine-similarity", () => computeCosineSimilarity(a, b))
-  .add("cosine-similarity", () => cosineSimilarity(a, b))
-  .add("tiny-tfidf cosineSimilarity()", () => Similarity.cosineSimilarity(a, b))
-  .on("cycle", (event) => console.log(String(event.target)))
-  .on("complete", function () {
-    console.log(`\nFastest is ${this.filter("fastest").map("name")}`);
-  })
-  .run();
+await run({ percentiles: false });
